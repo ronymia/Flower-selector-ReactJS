4f1ea4931cf0fec08d7c93968d9e97cec6b6f94d
ronymia <mdronymia.javascript@gmail.com>
2023-01-07 11:16:35 +0600

    shop product design complete

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,37 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import './Home.css';
 
 export default function Home() {
-    return (
-        <div>
+    const [products, setProducts] = useState([]);
 
-        </div>
+    useEffect(() => {
+        fetch('/data.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+
+    return (
+        <section className="container">
+            <section className="shop-container">
+                {
+                    products.map(product =>
+                        <article className="product">
+                            <figure>
+                                <img src={product.img} alt="" />
+                            </figure>
+                            <div className="product__details">
+                                <h3>{product.name}</h3>
+                                <p>${product.price} <del><span>$15.99</span></del></p>
+                            </div>
+                            <button type="button"
+                                className="product__btn"
+                            >Add To Cart</button>
+                        </article>)
+                }
+            </section>
+            <aside className="cart-container">
+                <h1>cart</h1>
+            </aside>
+        </section>
     )
 }
